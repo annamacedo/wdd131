dbbcc55c25264915195262e54f18cd83ad56ccbd
annamacedo <dar24005@byui.edu>
2024-07-11 16:30:22 -0300

    w04

--- scripts/form.js
+++ scripts/form.js
@@ -11,46 +11,28 @@ document.addEventListener('DOMContentLoaded', () => {
     authorSpan.textContent = '🌍 Anna Paula Macedo da Rosa 🌍';
     locationSpan.textContent = 'Rio Grande do Sul, Brazil';
     lastModifiedSpan.textContent = lastModified;
+
+    const productNameSelect = document.getElementById('productName');
+    const products = [
+        { id: 'fc-1888', name: "flux capacitor", 'avg-rating': 4.5 },
+        { id: 'fc-2050', name: "power laces", 'avg-rating': 4.7 },
+        { id: 'fs-1987', name: "time circuits", 'avg-rating': 3.5 },
+        { id: 'ac-2000', name: "low voltage reactor", 'avg-rating': 3.9 },
+        { id: 'jj-1969', name: "warp equalizer", 'avg-rating': 5.0 }
+    ];
+
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.textContent = `${product.name} (Rating: ${product['avg-rating']})`;
+        productNameSelect.appendChild(option);
+    });
 });
 
-const products = [
-    {
-        id: 'fc-1888',
-        name: "flux capacitor",
-        'avg-rating': 4.5
-    },
-    {
-        id: 'fc-2050',
-        name: "power laces",
-        'avg-rating': 4.7
-    },
-    {
-        id: 'fs-1987',
-        name: "time circuits",
-        'avg-rating': 3.5
-    },
-    {
-        id: 'ac-2000',
-        name: "low voltage reactor",
-        'avg-rating': 3.9
-    },
-    {
-        id: 'jj-1969',
-        name: "warp equalizer",
-        'avg-rating': 5.0
-    }
-];
-
 function incrementReviewCounter() {
-    
     if (typeof(Storage) !== "undefined") {
-        var count = localStorage.getItem("reviewCount");
-        if (!count) {
-            count = 0;
-        } else {
-            count = parseInt(count); 
-        }
-    
+        let count = localStorage.getItem("reviewCount");
+        count = count ? parseInt(count) : 0;
         count++;
         localStorage.setItem("reviewCount", count);
     } else {
