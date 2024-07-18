bd610c1eb11a953fea3805ee3b1a384df626c972
annamacedo <dar24005@byui.edu>
2024-07-18 12:49:06 -0300

    w06

--- project/scripts/rafting.js
+++ project/scripts/rafting.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const yearSpan = document.getElementById('year');
-    const authorSpan = document.getElementById('author');
-    const locationSpan = document.getElementById('location');
-    const lastModifiedSpan = document.getElementById('last-modified');
-
-    const currentYear = new Date().getFullYear();
-    const lastModified = document.lastModified;
-
-    yearSpan.textContent = currentYear;
-    authorSpan.textContent = '🌍 Anna Paula Macedo da Rosa 🌍';
-    locationSpan.textContent = 'Rio Grande do Sul, Brazil';
-    lastModifiedSpan.textContent = lastModified;
-});
-
-const hamButton = document.querySelector('.hamburguer'); 
+    const hamButton = document.querySelector('#menu'); 
     const navigation = document.querySelector('.navigation'); 
 
     hamButton.addEventListener('click', () => {
         navigation.classList.toggle('open'); 
         hamButton.classList.toggle('open'); 
     });
+
+    const yearSpan = document.getElementById('year'); 
+    const authorSpan = document.getElementById('author'); 
+    const locationSpan = document.getElementById('location'); 
+    const lastModifiedSpan = document.getElementById('last-modified'); 
+
+    const currentYear = new Date().getFullYear(); 
+    const lastModified = document.lastModified; 
+
+    yearSpan.textContent = currentYear; 
+    authorSpan.textContent = '🌍 Anna Paula Macedo da Rosa 🌍'; 
+    locationSpan.textContent = 'Rio Grande do Sul, Brazil'; 
+    lastModifiedSpan.textContent = lastModified; 
+});
